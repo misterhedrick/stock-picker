976754b4652b366835eb72e73fda105a6173a550
misterhedrick <33662070+misterhedrick@users.noreply.github.com>
2025-12-19 06:50:11 -0500

    Update stock-picker.py

--- stock-picker.py
+++ stock-picker.py
@@ -1,3 +1,11 @@
 import sys
+import os
+
+summary = os.environ["GITHUB_STEP_SUMMARY"]
+
+with open(summary, "a") as f:
+    f.write("## Script Results\n")
+    f.write("- Users: 42\n")
+    f.write("- Status: OK\n")
 
 print("Argument:", sys.argv[1])
